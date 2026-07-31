5953463e2f0f9f7523c1e2761f4fcda10b9c4f05
Massiano <Massiano@users.noreply.github.com>
2026-07-31 19:04:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import hashlib, json, os, threading, time
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
 app = Flask(__name__)
